67904a72edd18dd83cf3ab26bf9f4d7022ff38a2
Alan Storm <astorm@alanstorm.com>
2013-06-07 13:49:21 -0700

    linted inject.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 var options = {
     browser: true,
     indent: 4,
-    predef: ['tinymce', 'tinyMCE', 'setUserSetting']
+    predef: ['tinymce', 'tinyMCE', 'setUserSetting', 'toMarkdown', 'Markdown'],
+    maxlen: 900
 };
